acd075effe65d9c340160b97fa4c73e37516baf2
Taylan <taylansilva0402@gmail.com>
2026-07-01 13:39:34 -0300

    style: estilizando card da trilha e tela de trilhas

--- src/components/features/trails/Trail.jsx
+++ src/components/features/trails/Trail.jsx
@@ -4,10 +4,24 @@ import styles from "./Trail.module.css";
 
 export default function Trail({ trilha }) {
     return (
-        <div className={styles.trailContainer}>
-            <h2 className={styles.trailTitle}><FaRegFolderOpen size={24} className={styles.trailIcon} /> {trilha.titulo}</h2>
+        <div
+            className={styles.trailContainer}
+            style={{ "--trail-color": trilha.cor || "var(--text-color2)" }}
+        >
+            <div className={styles.trailHeader}>
+                <span className={styles.iconBox}>
+                    <FaRegFolderOpen size={20} className={styles.trailIcon} />
+                </span>
+
+                <h2 className={styles.trailTitle}>{trilha.titulo}</h2>
+            </div>
+
             <p className={styles.trailDescription}>{trilha.descricao}</p>
-            <RxOpenInNewWindow size={20} className={styles.trailOpenIcon} />
+
+            <div className={styles.trailFooter}>
+                <span>Ver trilha</span>
+                <RxOpenInNewWindow size={18} className={styles.trailOpenIcon} />
+            </div>
         </div>
-    )
+    );
 }
