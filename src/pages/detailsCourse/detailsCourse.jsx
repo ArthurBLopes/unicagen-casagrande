c1f32fabdb536a31d3f1faf669435816921ca493
Taylan <taylansilva0402@gmail.com>
2026-07-02 12:14:23 -0300

    feat: implementando página de detalhes do curso

--- src/pages/detailsCourse/detailsCourse.jsx
+++ src/pages/detailsCourse/detailsCourse.jsx
@@ -11,13 +11,36 @@ export default function DetailsCourse() {
     useScrollTop();
     console.log("Curso encontrado:", curso);
 
+    const linkMaterialVideo = curso?.linkConteudo?.includes("https://youtu.be") ? "video" : "documento";
+    console.log("Tipo de material:", linkMaterialVideo);
+
+    function getYouTubeEmbedUrl(url) {
+        const regex = /(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)([^&?/]+)/;
+        const match = url.match(regex);
+
+        if (!match) return "";
+
+        return `https://www.youtube.com/embed/${match[1]}`;
+    }
+
     return (
         <div className={styles.detalhesPage}>
             <BackPage />
             <div className={styles.cursoDetalhes}>
                 <p className={styles.titulo}>{curso?.titulo.toUpperCase()}</p>
                 <div className={styles.cursoContainer}>
-                    <img src={curso?.imagem} alt={curso?.titulo} className={styles.cursoImagem} />
+                    {curso?.linkConteudo && linkMaterialVideo === "video" && (
+                        <div className={styles.cursoVideo}>
+                            <iframe
+                                src={getYouTubeEmbedUrl(curso.linkConteudo)}
+                                title={curso.titulo}
+                                frameBorder="0"
+                            ></iframe>
+                        </div>
+                    )}
+                    {curso?.linkConteudo && linkMaterialVideo === "documento" && (
+                        <img src={curso.imagem} alt={curso.titulo} className={styles.cursoImagem} />
+                    )}
                     <div className={styles.cursoConteudo}>
                         <p className={styles.cursoTrilha}>{curso?.trilha}</p>
                         <h1 className={styles.cursoTitulo}>{curso?.titulo}</h1>
