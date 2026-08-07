d6a50141d011203e4dd133686a0932c08c5466ab
Taylan <taylansilva0402@gmail.com>
2026-08-07 12:22:04 -0300

    feat: criando opções da página de gerenciamento

---
+++ src/mocks/manager/mockOptions.jsx
@@ -0,0 +1,24 @@
+import { UserRound, Tag, LibraryBig, GraduationCap } from "lucide-react";
+
+export const opcoes = [
+    {
+        titulo: "TREINAMENTOS",
+        icon: <LibraryBig />,
+        caminho: "/teste",
+    },
+    {
+        titulo: "TRILHAS",
+        icon: <GraduationCap />,
+        caminho: "/teste",
+    },
+    {
+        titulo: "TAGS",
+        icon: <Tag />,
+        caminho: "/teste",
+    },
+    {
+        titulo: "USUÁRIOS",
+        icon: <UserRound />,
+        caminho: "/teste",
+    }
+]
--- src/pages/manager/managerHome/managerHome.jsx
+++ src/pages/manager/managerHome/managerHome.jsx
@@ -1,11 +1,29 @@
 import styles from "./managerHome.module.css"
+import { opcoes } from "../../../mocks/manager/mockOptions"
+import { NavLink } from "react-router-dom";
 
 export default function ManagerHome() {
+
+    console.log(opcoes)
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <h1>Gerenciamento</h1>
                 <p>Insira, edite ou remova informações da plataforma, essa é a página de gerenciamento da Universidade Casagrande.</p>
+                <div className={styles.opcoesContainer}>
+                    {opcoes.map((opcao) => (
+                        <NavLink
+                            key={opcao.titulo}
+                            to={opcao.caminho}
+                            className={styles.opcao}
+                            title={opcao.titulo}
+                        >
+                            <span className={styles.icon}>{opcao.icon}</span>
+                            <span className={styles.titulo}>{opcao.titulo}</span>
+                        </NavLink>
+                    ))}
+                </div>
             </main>
         </div>
     )
